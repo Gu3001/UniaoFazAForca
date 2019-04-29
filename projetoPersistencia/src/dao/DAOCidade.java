8d335d63cba47c8e97bfcd38b4d1af8c89cbf2a2
Luiz Gustavo <37215681+Gu3001@users.noreply.github.com>
2019-04-29 16:23:48 -0300

    adicionado dao cidade
    dao cidade e estado completos

--- projetoPersistencia/src/dao/DAOCidade.java
+++ projetoPersistencia/src/dao/DAOCidade.java
@@ -1,5 +1,7 @@
 package dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
@@ -28,5 +30,57 @@ public class DAOCidade {
 			gerenciador.close();
 		}
 		return cidade;
+	}
+	public Cidade excluir(Cidade cidade) {
+		try {
+			EntityManagerFactory fabrica = Fabrica.get();
+			gerenciador = fabrica.createEntityManager();
+			transacao = gerenciador.getTransaction();
+			
+			transacao.begin();
+			cidade = gerenciador.find(Cidade.class, cidade.getId());
+			gerenciador.remove(cidade);
+			transacao.commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+			transacao.rollback();
+		}finally{
+			gerenciador.close();
+		}
+		return cidade;
+	}
+	public Cidade alterar(Cidade cidade) {
+		try {
+			EntityManagerFactory fabrica = Fabrica.get();
+			gerenciador = fabrica.createEntityManager();
+			transacao = gerenciador.getTransaction();
+			
+			transacao.begin();
+			gerenciador.merge(cidade);
+			transacao.commit();
+			
+		}catch(Exception e){
+			e.printStackTrace();
+			transacao.rollback();
+		}finally {
+			gerenciador.close();
+		}return cidade;
+	}
+	
+	public List<Cidade> buscar() {
+		try {
+			EntityManagerFactory fabrica = Fabrica.get();
+			gerenciador = fabrica.createEntityManager();
+			
+			return gerenciador.createQuery("from Cidade").getResultList();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			transacao.rollback();
+		}finally {
+			gerenciador.close();
+		}
+		
+		return null;
 	}
 }
--- projetoPersistencia/src/tela/TelaCidade.java
+++ projetoPersistencia/src/tela/TelaCidade.java
@@ -22,6 +22,11 @@ public class TelaCidade {
 	private Principal principal = new Principal();
 	
 	public TelaCidade() {
+		Cidade cidade = new Cidade();
+		DAOEstado daoestado = new DAOEstado();
+		Estado estado = new Estado();
+		
+//		listarCadastros listaEst = new listarCadastros();
 		
 		String r = "s";
 		int op = 0;
@@ -33,34 +38,50 @@ public class TelaCidade {
 			}while(op < 1 && op > 4);
 			
 			if(op == 1) {
-				Cidade cidade = new Cidade();
-				DAOEstado daoestado = new DAOEstado();
-				Estado estado = new Estado();
-				
 				System.out.println("Nome da cidade: ");
 				String nome = scan.nextLine();
 				cidade.setNome(nome);
-				
-				
-				List<Estado> lista = daoestado.buscar();
-				for (Estado est : lista) {
-					System.out.println("Id: "+est.getId()+" Estado: "+est.getNome()+" - "+est.getSigla());
-				}
+				listarCadastrosEstado();
 				
 				System.out.println("Insira o ID: ");
 				long id = scan.nextLong();
 				estado.setId(id);
 				cidade.setEstado(estado);
 				daocidade.inserir(cidade);
+				principal.main(null);
+				//colocar para retornar para principal no final de cada um
 			}if (op ==2) {
-				
+				listarCadastrosCidade();
+				System.out.println("EXCLUIR CADASTRO");
+				System.out.println("Insira o id do Estado a ser excluido: ");
+				cidade.setId( scan.nextLong() );
+				daocidade.excluir(cidade);
+				principal.main(null);
 			}
 			if (op ==3) {
-			
+				listarCadastrosCidade();
+				System.out.println("ALTERAR CADASTRO");
+				System.out.println("Insira o Id do cadastro a ser alterado");	
+				cidade.setId(scan.nextLong());
+				System.out.println("Novo nome: ");
+				cidade.setNome(scan.next());
+				
+				listarCadastrosEstado();
+				System.out.println("Novo Estado, id: ");
+				long id = scan.nextLong();
+				
+				System.out.println("Alterando...");
+				
+				estado.setId(id);
+				cidade.setEstado(estado);
+				daocidade.alterar(cidade);
+				
+				
+				principal.main(null);
 			}
 			
 			if(op == 4) {
-			
+				listarCadastrosCidade();
 				
 			}
 
@@ -70,6 +91,26 @@ public class TelaCidade {
 			r = scan.nextLine();
 			
 		}while(r.contentEquals("s"));
+	}
+	public void listarCadastrosCidade() {
+		
+		
+		System.out.println("-----------------------------------------");
+		System.out.println("LISTAR TODAS CIDADES");
+		List<Cidade> cid = daocidade.buscar(); //estudar melhor o percorrer da lista
+		
+		for(Cidade cidade : cid) {
+			System.out.println("Id: "+cidade.getId()+" Cidade: "+cidade.getNome()+" Estado: "+cidade.getEstado());
+		}
+	}
+	public void listarCadastrosEstado() {
+		DAOEstado daoestado = new DAOEstado();
+		Estado estado = new Estado();
+		List<Estado> lista = daoestado.buscar();
+		
+		for (Estado est : lista) {
+			System.out.println("Id: "+est.getId()+" Estado: "+est.getNome()+" - "+est.getSigla());
+		}
 	}
 	
 	//Só para fazer a listar todos os cadastros em operações
